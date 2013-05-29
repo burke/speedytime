3fdd5f4fa53fa65bf7597019ef8620734f3d6779
Burke Libbey <burke@libbey.me>
2013-05-29 16:20:47 -0500

    added current_f method for returning as a double

--- ext/speedytime/speedytime.c
+++ ext/speedytime/speedytime.c
@@ -8,8 +8,24 @@ VALUE speedytime_current(void)
   return INT2FIX(tv.tv_sec);
 }
 
+VALUE speedytime_current_i(void)
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return INT2FIX(tv.tv_sec);
+}
+
+VALUE speedytime_current_f(void)
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  double prec = tv.tv_sec + (tv.tv_usec / 1000000.0);
+  return DBL2NUM(prec);
+}
+
 void Init_speedytime(void)
 {
   VALUE mSpeedytime = rb_define_module("Speedytime");
-  rb_define_module_function(mSpeedytime, "current", speedytime_current, -1);
+  rb_define_module_function(mSpeedytime, "current", speedytime_current_i, 0);
+  rb_define_module_function(mSpeedytime, "current_f", speedytime_current_f, 0);
 }
